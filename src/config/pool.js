2bd6ea4908689b48e5e92a5a1c41cb5458c57130
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-05-24 16:46:34 +0100

    Work on ca-certificates.crt to make ssl work

--- src/config/pool.js
+++ src/config/pool.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-const fs = require("fs");
-const path = require("path");
-
-const caCert = fs.readFileSync(path.join(__dirname, "ca.pem")).toString();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true,
-    ca: caCert,
   },
 });
 
