687f614131fcbf118e1365a29dc8fad23c707259
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-05-24 16:57:54 +0100

    Set sll rejectUnathorized to true

--- src/config/pool.js
+++ src/config/pool.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+const fs = require('fs');
+const path = require('path');
+const { Pool } = require('pg');
+
+const caCert = fs.readFileSync(path.join(__dirname, 'config', 'ca.pem')).toString();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true,
+    ca: caCert,
   },
 });
 
